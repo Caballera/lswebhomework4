ecf84d9a08c33284ec6265e32d497a170f695164
Melvin Peepers <peepersm@ninjasitm.com>
2017-06-01 20:08:02 -0400

    added HTML and Script

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.getElementById("whatever").innerHTML = "Whatever we want!";
+
+// document.getElementById("nickname").innerHTML = "Austin";
+
+/*
+var el = document.createElement('div');
+el.innerHTML = "<p>Hello World!</p>"
+
+document.body.appendChild(el);
+
+var el2 = document.querySelector('div');
+var newEl = document.createElement('p');
+*/
+
+
+document.body.style.backgroundColor = "red";
+
+document.getElementById("whatever").style.fontFamily = "sans-serif";
